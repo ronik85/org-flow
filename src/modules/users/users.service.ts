6e0eebcee34f78bf5cb08cc2cb30aecc8b6a6abc
ronik85 <ronik.kalanouria@gmail.com>
2026-02-03 17:03:15 +0530

    feat: Implement user pagination, email uniqueness validation, and password hashing for create and update operations.

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -2,8 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { UserNotFoundException } from 'src/common/exceptions/user-not-found.exception';
+import { EmailAlreadyExistsException } from 'src/common/exceptions/email-already-exists.exception';
+import * as bcrypt from 'bcrypt';
 import { Repository } from 'typeorm';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -14,7 +17,12 @@ export class UsersService {
   ) { }
 
   findAll(query: PaginationQueryDto) {
-    return this.userRepository.find();
+    const { page = 1, limit = 10 } = query;
+    const offset = (page - 1) * limit;
+    return this.userRepository.find({
+      skip: offset,
+      take: limit,
+    });
   }
 
   async findOne(id: string) {
@@ -31,13 +39,21 @@ export class UsersService {
     return this.userRepository.findOne({ where: { email } });
   }
 
-  async create(dto: Partial<User>) {
-    const user = this.userRepository.create(dto);
+  async create(dto: CreateUserDto) {
+    const existingUser = await this.findByEmail(dto.email);
+    if (existingUser) {
+      throw new EmailAlreadyExistsException();
+    }
+    const user: User = this.userRepository.create(dto);
+    user.password = await bcrypt.hash(dto.password, 10);
     return this.userRepository.save(user);
   }
 
   async update(id: string, dto: UpdateUserDto) {
     const user = await this.findOne(id);
+    if (dto.password) {
+      dto.password = await bcrypt.hash(dto.password, 10);
+    }
     Object.assign(user, dto);
     return this.userRepository.save(user);
   }
